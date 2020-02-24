3cef7f764ae266186830dff126901839cc1b56ed
Nate Hobi <nate.hobi@gmail.com>
2020-02-24 12:42:17 -0500

    Adds EN as the default language locale for nova

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,30 +1,38 @@
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return "";
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 Nova.booting((Vue, router) => {
-    Vue.component('index-multilingual-nova', require('./components/IndexField'));
-    Vue.component('detail-multilingual-nova', require('./components/DetailField'));
-    Vue.component('form-multilingual-nova', require('./components/FormField'));
+	Vue.component(
+		"index-multilingual-nova",
+		require("./components/IndexField")
+	);
+	Vue.component(
+		"detail-multilingual-nova",
+		require("./components/DetailField")
+	);
+	Vue.component("form-multilingual-nova", require("./components/FormField"));
 
-    Vue.component('language-selector', require('./components/LanguageSelector'));
+	Vue.component(
+		"language-selector",
+		require("./components/LanguageSelector")
+	);
 
-    router.addRoutes([
-        {
-            name: 'nova-language-tool',
-            path: '/nova-language-tool',
-            component: require('./components/Tool'),
-        },
-    ])
+	router.addRoutes([
+		{
+			name: "nova-language-tool",
+			path: "/nova-language-tool",
+			component: require("./components/Tool")
+		}
+	]);
 
-    let lang = getParameterByName('lang');
-    if (lang) {
-        Nova.request().defaults.headers['lang'] = lang;
-    }
-})
+	let lang = getParameterByName("lang");
+
+	Nova.request().defaults.headers["lang"] = lang ? lang : "EN";
+});
